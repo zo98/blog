def35ac6e0f41a67e971dcd7917e1668de5a4715
flying-98 <79616275+flying-98@users.noreply.github.com>
2021-08-12 18:13:40 +0800

    u

--- src/components/admin/article-edit.jsx
+++ src/components/admin/article-edit.jsx
@@ -14,6 +14,7 @@ import axios from "@/http/service";
 import React, { Component } from "react";
 import Menu from "@/components/menu/index.jsx";
 import Editor from "@/components/TinymceEditor.jsx";
+// import Editor from "@/components/CKEditor.jsx";
 class ArticleEdit extends Component {
   constructor(props) {
     super(props);
