61fbff818ed4f34dc85cc9050c4cc6ff0a84f887
flying-98 <79616275+flying-98@users.noreply.github.com>
2021-08-15 21:59:36 +0800

    a

--- pages/article/[id].jsx
+++ pages/article/[id].jsx
@@ -24,12 +24,12 @@ export default function Index(props) {
           <ul style={{ position: "fixed" }}>
             <li>
               <SiderBar title="分类目录">
-                <SiderBarList type="classify" />
+                <SiderBarList type="classify" data={props.classify_data} />
               </SiderBar>
             </li>
             <li style={{ marginTop: "15px" }}>
               <SiderBar title="近期文章">
-                <SiderBarList type="article" />
+                <SiderBarList type="article" data={props.articles_data} />
               </SiderBar>
             </li>
           </ul>
@@ -41,10 +41,33 @@ export default function Index(props) {
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
-  const { data } = await axios({
-    url: "http://localhost:8000/api/article/getArticle",
-    method: "GET",
-    params: { id },
-  });
-  return { props: { data: data.data } };
+  const article = fetch(
+    `http://localhost:8000/api/article/getArticle?id=${id}`
+  ).then((res) => res.json());
+
+  const articles = fetch("http://localhost:8000/api/article/getArticle").then(
+    (res) => res.json()
+  );
+  const classify = fetch(
+    "http://localhost:8000/api/classify/hotClassify?pageSize=5"
+  ).then((res) => res.json());
+  try {
+    const [res1, res2, res3] = await Promise.all([article, classify, articles]);
+    let data = [],
+      classify_data = [],
+      articles_data = [];
+    if (res1.code === 1) {
+      data = res1.data;
+    }
+    if (res2.code === 1) {
+      classify_data = res2.data.records;
+    }
+    if (res3.code === 1) {
+      articles_data = res3.data.records;
+    }
+    return { props: { data, classify_data, articles_data } };
+  } catch (error) {
+    console.log(error);
+    return { props: { data: [], classify_data: [], articles_data: [] } };
+  }
 }
